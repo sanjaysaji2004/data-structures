ef276dd06421ea4322fdb9b040d8bbb2e7726634
sanjaysaji2004 <123439054+sanjaysaji2004@users.noreply.github.com>
2023-05-03 10:12:40 +0530

    Add files via upload

---
+++ ODD OR EVEN.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter an integer:");
+	scanf("%d",&num);
+	if("num/2==0")
+	{
+	printf("it is even");
+	}	
+	else
+    {
+	printf("it is odd");
+	}
+}
